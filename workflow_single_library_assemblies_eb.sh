f57d24ae17d2289eb9e30bd932c7264ee5ac74ff
Evan Barba <35745523+ewbarba@users.noreply.github.com>
2019-11-26 09:56:37 -1000

    Update workflow_single_library_assemblies_eb.sh

--- workflow_single_library_assemblies_eb.sh
+++ workflow_single_library_assemblies_eb.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+#set genome as eukaryote or prokaryote
+G_size="EUK"
+#G_size="PRO"
+
+#define desired total number of cores to use
 NUMPROC=48
+#define max memory (in GB) to use
 MEMORY=350
+#define number of 'splits' for large jobs - for small genome assembly, you can split resources to save time
+if [ $G_size = 'EUK' ]; then SPLITSa=1; fi
+if [ $G_size = 'PRO' ]; then SPLITSa=3; fi
+
+#set project path
 PROJ=${PWD##*/}
 #USR_KMER="71,81,91,99,121,127"
 
@@ -28,8 +39,8 @@ if [ ! -d "quast" ]; then mkdir quast; fi
 #####TRIMMING#####-----------------------------------------------------------
 if test -n "$(find ./raw -name '*R2*' -print -quit)"; then
     parallel -j $NUMPROC trim_galore --paired --illumina --fastqc -o trimming/ ::: ` find  ./raw  -name "*_R1*.f*.gz" ` :::+ ` find  ./raw  -name "*_R2*.f*.gz" `
-    FWDS=`find ./trimming -name "*R1*.gz" | sort | uniq`
-    REVS=(` find ./trimming -name "*R2*.gz" | sort | uniq `)
+    #FWDS=`find ./trimming -name "*R1*.gz" | sort | uniq`
+    #REVS=(` find ./trimming -name "*R2*.gz" | sort | uniq `)
 else
    parallel -j $NUMPROC trim_galore --illumina --fastqc -o trimming/ ::: ` find  ./raw  -name "*_R1*.f*.gz" `
    FWDS=`find ./trimming -name "*R1*.gz" | sort | uniq`
@@ -38,36 +49,29 @@ fi
 #####SPADES ASSEMBLY#####
 if test -n "$(find ./raw -name '*R2*' -print -quit)"; then
     echo reverse reads found. proceeding with paired end assembly using $NUMPROC cores and $MEMORY GB memory
-    for file1 in $FWDS; do
-        file2=${file1/R1_001_val_1/R2_001_val_2}
-        tempname="${file1/.\/trimming\//}"
-        outname="${tempname/_*_R*.fq.gz/}_output"
-        echo $outname
-        #spades.py --careful -k 71,81,91,99,121,127 -1 $file1 -2 $file2 -t $NUMPROC -m $MEMORY -o ./assembly/${outname}_usr_kmer 
-        spades.py --careful -1 $file1 -2 $file2 -t $NUMPROC -m $MEMORY -o ./assembly/${outname}_default_kmer
-    done
+    parallel -j $SPLITSa spades.py --careful -1 {1} -2 {2} -t $(( $NUMPROC / $SPLITSa )) -m $(( $MEMORY / $SPLITSa )) -o ./assembly/{3}_default_kmer ::: \
+    ` find ./trimming -name "*R1*.gz" | sort | uniq ` :::+ \
+    ` find ./trimming -name "*R2*.gz" | sort | uniq ` :::+ \
+    ` find ./trimming -name "*R1*.gz" | sort | uniq | cut -d "/" -f3 | cut -d "_" -f1 `
 else
     echo no reverse reads found. proceeding with single end assebmly using $NUMPROC cores and $MEMORY GB memory
-    for file1 in $FWDS; do
-        tempname="${file1/.\/trimming\//}"
-        outname="${tempname/_*_R*.fq.gz/}_output"
-        #spades.py --careful -k 71,81,91,99,121,127 -1 $file1 -t $NUMPROC -m $MEMORY -o ./assembly/${outname}_usr_kmer
-        spades.py --careful -1 $file1 -t $NUMPROC -m $MEMORY -o ./assembly/${outname}_default_kmer
-    done
+    parallel -j $SPLITSa spades.py --careful -1 {1} -t $(( $NUMPROC / $SPLITSa )) -m $(( $MEMORY / $SPLITSa )) -o ./assembly/{3}_default_kmer ::: \
+    ` find ./trimming -name "*R1*.gz" | sort | uniq ` :::+ \
+    ` find ./trimming -name "*R1*.gz" | sort | uniq | cut -d "/" -f3 | cut -d "_" -f1 `
 fi
 
 #####QUAST#####-----------------------------------------------------------
 #per KMER
-for genome in `find ./assembly -maxdepth 1 -name "*kmer" -type d | cut -d "/" -f3`; do
-    KMERS=(` find ./assembly/$genome/ -maxdepth 1 -name "K*" | cut -d "/" -f4 | sort | uniq `)
-    cp ./assembly/${genome}/contigs.fasta ./quast/"${genome}_contigs.fasta"
-         for KMER in `find ./assembly/$genome/ -maxdepth 1 -name "K*" | cut -d "/" -f4 | sort | uniq`; do
-             cp ./assembly/${genome}/${KMER}/final_contigs.fasta ./quast/${genome}_${KMER}.fasta
-        done
-    by_kmer=("${KMERS[@]/#/./quast/${genome}_}")
-    by_kmer_files=("${by_kmer[@]/%/.fasta}")
-    quast.py -e -t $NUMPROC -m $MEMORY ./quast/${genome}_contigs.fasta "${by_kmer_files[@]}" -o ./quast/${genome}_results/
-done
+# for genome in `find ./assembly -maxdepth 1 -name "*kmer" -type d | cut -d "/" -f3`; do
+#     KMERS=(` find ./assembly/$genome/ -maxdepth 1 -name "K*" | cut -d "/" -f4 | sort | uniq `)
+#     cp ./assembly/${genome}/contigs.fasta ./quast/"${genome}_contigs.fasta"
+#          for KMER in `find ./assembly/$genome/ -maxdepth 1 -name "K*" | cut -d "/" -f4 | sort | uniq`; do
+#              cp ./assembly/${genome}/${KMER}/final_contigs.fasta ./quast/${genome}_${KMER}.fasta
+#         done
+#     by_kmer=("${KMERS[@]/#/./quast/${genome}_}")
+#     by_kmer_files=("${by_kmer[@]/%/.fasta}")
+#     quast.py -e -t $NUMPROC -m $MEMORY ./quast/${genome}_contigs.fasta "${by_kmer_files[@]}" -o ./quast/${genome}_results/
+# done
 
 #all default KMER selection
 #all_user=(` find ./quast/*usr*contigs.fasta  -type f `)
@@ -76,8 +80,17 @@ done
 #spades default settings
 #all default KMER selection
 
-all_default=(` find ./quast/*default*contigs.fasta  -type f `)
-quast.py -e -t $NUMPROC -m $MEMORY "${all_default[@]}" -o ./quast/default_spades/  
+#copy over contig outputs for 
+parallel -j $NUMPROC cp ./assembly/{}/contigs.fasta ./quast_test/{}_contigs.fasta ::: \
+` find ./assembly -maxdepth 1 -type d | sort | uniq | cut -d "/" -f3 `
+
+all_default=(` find ./quast_test/*contigs.fasta  -type f `)
+
+if [ $G_size = "EUK" ]; then
+    quast.py --large -e -k -t $NUMPROC -m $MEMORY "${all_default[@]}" -o ./quast_test/default_spades/  ; fi
+
+if [ $G_size = "PRO" ]; then
+    quast.py --gene-finding -b -t $NUMPROC -m $MEMORY "${all_default[@]}" -o ./quast_test/default_spades/  ; fi
 
 #########STOP######-----------------------------------------------------------
 #before proceeding: evaluate your quast results. Choose desired de novo, copy to ./assemly with a name to include *chosen.fasta
